594c7c47825aa00fafc3635ebc8b81f109ba4e25
Jeff Chan <cp4c5ism@gmail.com>
2023-09-28 18:13:39 -0400

    should be the final update

--- Adv lab 1/lab 2/lab 2.py
+++ Adv lab 1/lab 2/lab 2.py
@@ -1,20 +1,27 @@
 #lab 2 plot
+#all print comments were typed in to check list numbers and csv names for columns
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 path = "https://raw.githubusercontent.com/Ge-tector/Adv-lab/main/Adv%20lab%201/lab%202/data%20for%20lab%202.csv"
 data = np.genfromtxt(path, delimiter=",", names=True)
+#print(data.dtype.names)
 
 millisec = data["Time"]
-V_2 = data["Analog"]
+A0 = data["Analog"]
+V_2 = np.divide(A0, 1023) * 5
+#print(V_2)
 
-time = 1000*millisec
+time = millisec/1000
 V_in = 5
 R_2 = 3300
+#print(time)
 
 R_1 = np.divide(V_in*R_2,V_2) - R_2
+#print(R_1)
 
+#The following values were obtained from the datasheet
 R_25 = 3300
 A_1 = 3.354016E-03
 B_1 = 2.569850E-04
@@ -24,10 +31,11 @@ D_1 = 6.383091E-08
 Rratio = np.divide(R_1, R_25)
 
 T = (A_1 + (B_1*np.log(Rratio)) + (C_1*(np.log(Rratio))**2) + (D_1*(np.log(Rratio))))**(-1)
+#print(T)
 
 fig, ax = plt.subplots()
 ax.set_xlim([0, 81]) 
-ax.set_ylim([273, 333])
+ax.set_ylim([295, 305])
 ax.set_title('Relation between Temperature and time')
 ax.set_xlabel("Time (s)")
 ax.set_ylabel("Temperature (K)")
